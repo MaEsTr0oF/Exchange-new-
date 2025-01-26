d2894c8050f4395fe54e8aa44073236cfe7300cb
MaEsTr0oF <114798814+MaEsTr0oF@users.noreply.github.com>
2025-01-26 21:16:01 +0300

    update scripts

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -24,10 +24,11 @@ document.addEventListener('DOMContentLoaded', async () => {
 	};
 
 	burger.addEventListener('click', toggleMenu);
-	const data = fetchCurrencyRates();
+	const data = await fetchCurrencyRates();
 	console.log(data);
 	while(data==undefined){
-		data = fetchCurrencyRates();
+		data = await fetchCurrencyRates();
+		console.log("Не работает");
 	}
 	const moveToBurger = () => {
 		const width = window.innerWidth;
@@ -86,8 +87,6 @@ document.addEventListener('DOMContentLoaded', async () => {
 		}
 	}
 	// Функция для обновления значений покупки и продажи
-	console.log("Не работает");
-	console.log(data);
 	async function fillSpan() {
 		for (const currency in data) {
 			const buyingElement = document.querySelector(`.buying-${currency}`);
